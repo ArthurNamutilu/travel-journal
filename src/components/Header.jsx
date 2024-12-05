8ebfa34aec8c4d41bee09e6f7f4cd5e1511b6cdd
ArthurNamutilu <arthurggnamutilu@gmail.com>
2024-12-05 22:07:00 +0300

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import globeImage from "../globe.png";
+
+const Header = () => (
+  <header>
+    <img src={globeImage} alt="globe image" />
+    <h1>my travel journal</h1>
+  </header>
+);
+
+export default Header;
